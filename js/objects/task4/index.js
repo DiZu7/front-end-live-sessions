184ba48478739f7f9dd977e6340bc65229a8cfbb
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-13 14:49:39 +0200

    test

--- js/objects/task4/index.js
+++ js/objects/task4/index.js
@@ -10,13 +10,91 @@
 // 1. get keys obj1
 // 2. get keys obj2
 // 3. compare keys length and return false if length is not equal
-// 4. iterate keys obj1 and chech if the value of obj1key === value of obj2key
+// 4. iterate keys obj1 and check if the value of obj1key === value of obj2key
+
+// option-1 GOOD
+// function compareObjects(firstObj, secondObj) {
+//   return Object.keys({ ...firstObj, ...secondObj }).every(key => firstObj[key] === secondObj[key]);
+// }
+
+// option-2 NORM
+// function compareObjects(firstObj, secondObj) {
+//   return !Object.keys({ ...firstObj, ...secondObj }).some(key => firstObj[key] !== secondObj[key]);
+// }
+
+// option-3 BAD
+// function compareObjects(firstObj, secondObj) {
+//   const biggerObj =
+//     JSON.stringify(firstObj).length > JSON.stringify(secondObj).length ? firstObj : secondObj;
+//   return !Object.keys(biggerObj).some(key => firstObj[key] !== secondObj[key]);
+// }
+
+// option-4
+// NORM
+// const compareObjects = (firstObj, secondObj) => {
+//   const firstArr = Object.keys(firstObj);
+//   const secondArr = Object.keys(secondObj);
+//   if (firstArr.length !== secondArr.length) {
+//     return false;
+//   }
+//   return !firstArr.some(key => firstObj[key] !== secondObj[key]);
+// };
+
+// option-4 refactored
+// const compareObjects = (firstObj, secondObj) => {
+//   const firstObjKeys = Object.keys(firstObj);
+//   if (firstObjKeys.length !== Object.keys(secondObj).length) {
+//     return false;
+//   }
+//   return firstObjKeys.every(key => firstObj[key] !== secondObj[key]);
+// };
+
+// option-5 BAD
+// const compareObjects2 = (firstObj, secondObj) =>
+//   JSON.stringify(Object.entries(firstObj).sort()) ===
+//   JSON.stringify(Object.entries(secondObj).sort());
+
+// option-6 BAD
+// function compareObjects(firstObj, secondObj) {
+//   const firstArray = Object.keys(firstObj);
+//   if (firstArray.length !== Object.keys(secondObj).length) {
+//     return false;
+//   }
+//   return firstArray.reduce((acc, element) => {
+//     if (firstObj[element] !== secondObj[element]) {
+//       acc = false;
+//     };
+//     return acc;
+//   }, true);
+// };
+
+// option-7
+// BAD
+// const compareObjects = (firstObj, secondObj) =>
+//   Object.keys(firstObj).reduce(
+//     (acc, key, index) => (firstObj[key] === secondObj[key] && index === acc ? acc + 1 : -1),
+//     0,
+//   ) === Object.keys(secondObj).length;
+
+// option-9 BAD
+// function compareObjects(firstObj, secondObj) {
+//   const ordered = obj =>
+//     Object.keys(obj)
+//       .sort()
+//       .reduce((acc, el) => {
+//         acc[el] = obj[el];
+//         console.log(acc);
+//         return acc;
+//       }, {});
+//   const sortFirstObj = ordered(firstObj);
+//   const sortSecondObj = ordered(secondObj);
+//   return JSON.stringify(sortFirstObj) === JSON.stringify(sortSecondObj);
+// }
 
 function compareObjects(firstObj, secondObj) {
   const firstObjKeys = Object.keys(firstObj);
-  const secondObjKeys = Object.keys(secondObj);
 
-  if (firstObjKeys.length !== secondObjKeys.length) {
+  if (firstObjKeys.length !== Object.keys(secondObj).length) {
     return false;
   }
 
